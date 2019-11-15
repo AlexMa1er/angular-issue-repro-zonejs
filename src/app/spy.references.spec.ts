13d3c341d069f091d3888d6878961d9ebfebd99f
Alexander Maier <alexander.maier@zeiss.com>
2019-11-15 07:57:39 +0100

    chore(*) add spy spec sample

--- src/app/spy.references.spec.ts
+++ src/app/spy.references.spec.ts
@@ -1,10 +1,17 @@
 import * as fromRxJs from 'rxjs';
 
+declare var spyOnProperty: Function;
+
 describe('SpyReferences', () => {
 
-  it('should spy the reference', () => {
+  it('should a spyable reference', () => {
     const pd = Object.getOwnPropertyDescriptor(fromRxJs, 'interval');
     expect(pd.configurable).toBeTruthy();
+  });
+
+  it('should spy the rxjs-interval method reference', () => {
+    spyOnProperty(fromRxJs, 'interval', 'get').and.returnValue({});
+    expect(true).toBeTruthy();
   });
 
 });
