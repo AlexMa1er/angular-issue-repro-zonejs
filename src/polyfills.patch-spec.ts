e791cd949af2bef9652d8d533fbf8b1b4c2653a8
Alexander Maier <alexander.maier@zeiss.com>
2019-11-19 07:41:18 +0100

    fix(*) add polyfills.patch-spec.ts impl

---
+++ src/polyfills.patch-spec.ts
@@ -0,0 +1,10 @@
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+
+// (window as any).__Zone_disable_registerElement = true;
+// (window as any).__Zone_disable_EventTargetLegacy = true;
+import 'zone.js/dist/zone-legacy';
+// (window as any)['__zone_symbol__legacyPatch']();
+
+import './polyfills';
